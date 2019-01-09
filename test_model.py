b1bae920a1f644002071c0350d6b3a6cb35f8109
Ratan <ratanboddu@gmail.com>
2019-01-09 15:23:44 +0530

    Second Commit (Student CRUD)

--- test_model.py
+++ test_model.py
@@ -1,29 +1,47 @@
 import myflask
+import falcon
 
 
 #def test_student(new_home):
 #    assert new_home.name == "BodduBhai
+dict_add = {
+    "selectedid": 102,
+    "classleader": "Yes",
+    "name": "Viona"
+}
 
+dict_update_data = {
+    "newName": "Sanjay Boddu Bhai",
+    "oldClassId": 102,
+    "studentId": 1,
+    "classleader": "Yes",
+
+}
 
 def test_home(test_resp_code):
     abc = test_resp_code
-    resp = abc.post('/?name="Ratan"')
+    resp = abc.post('/', data=dict_add)
     assert resp.status_code == 200
+    #assert resp.status_code == 200
 
 
 def test_show_class(test_resp_code):
     abc = test_resp_code
-    resp = abc.post('/show_class/?id=102')
+    resp = abc.post('/show_class?id=100')
     assert resp.status_code == 200
 
 
 def test_show_update(test_resp_code):
     abc = test_resp_code
+    resp = abc.post('/update', data=dict_update_data)
+    assert resp.status_code == 302
+
+
+def test_show_delete(test_resp_code):
+    abc = test_resp_code
     resp = abc.post('/update')
-    assert resp.status_code == 500
+    assert resp.status_code == 302
 
-
-#def test_add():
 
 
 
