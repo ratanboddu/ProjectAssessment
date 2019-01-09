3d2c42a2e21e8cd76363e1d4ba34efc99cbf2e08
Ratan <ratanboddu@gmail.com>
2019-01-09 16:29:50 +0530

    Second Commit (Student CRUD)

--- conftest.py
+++ conftest.py
@@ -11,28 +11,16 @@ from flask.testing import FlaskClient
 # db = SQLAlchemy(app)
 #
 #
-# @pytest.fixture(scope='module')
-# def new_home():
-#     student = Student(id=45, name="BodduBhai", class_id=102, createdon="null", updatedon="null")
-#     #classdet = Class(id=102, name="10th B", class_leader=45, createdon="null", updatedon="null")
-#     return student
-#
-#
-# @pytest.fixture(scope='module')
-# def new_home_class(request):
-#     app = create_app(__name__, settings_override)
-#     testjust = myflask.app.test_client()
-#     #flask_app = flask.Flask(__name__)
-#     #testing_client = flask_app.test_client()
-#     ctx = myflask.app.app_context()
-#     ctx.push()
-#
-#     yield testjust
-#
-#     ctx.pop()
-#     #student = Student(id=45, name="BodduBhai", class_id=102, createdon="null", updatedon="null")
-#     #classdet = Class(id=102, name="10th B", class_leader=45, createdon="null", updatedon="null")
-#     #return classdet
+
+
+@pytest.fixture(scope="module")
+def create_db():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@localhost/test'
+    app.config['SECRET_KEY'] = "ratanboddu"
+    dbname = SQLAlchemy(app)
+    dbname.create_all()
+
 
 
 @pytest.fixture(scope='module')
--- test_model.py
+++ test_model.py
@@ -4,31 +4,56 @@ import falcon
 
 #def test_student(new_home):
 #    assert new_home.name == "BodduBhai
-dict_add = {
+dict_add_yes = {
     "selectedid": 102,
     "classleader": "Yes",
     "name": "Viona"
 }
-
-dict_update_data = {
-    "newName": "Sanjay Boddu Bhai",
+dict_add_no = {
+    "selectedid": 102,
+    "classleader": "No",
+    "name": "Viona"
+}
+dict_update_data_no = {
+    "newName": "Viona Gonsalves",
     "oldClassId": 102,
-    "studentId": 1,
+    "studentId": 57,
+    "classleader": "No",
+
+}
+dict_update_data_yes = {
+    "newName": "Rishi Kambil",
+    "oldClassId": 103,
+    "studentId": 57,
     "classleader": "Yes",
 
 }
 dict_delete_data = {
 
-    "studentId": 9
+    "studentid": 57
+
+}
+dict_delete_data_exception = {
+
+    "studentid": 57
 
 }
 
+dict_add_yes = {
+    "selectedid": 103,
+    "classleader": "Yes",
+    "name": "Rishi"
+}
 def test_home(test_resp_code):
     abc = test_resp_code
-    resp = abc.post('/', data=dict_add)
+    resp = abc.post('/', data=dict_add_no)
     assert resp.status_code == 200
-    #assert resp.status_code == 200
 
+
+def test_home(test_resp_code):
+    abc = test_resp_code
+    resp = abc.post('/', data=dict_add_yes)
+    assert resp.status_code == 200
 
 def test_show_class(test_resp_code):
     abc = test_resp_code
@@ -38,7 +63,14 @@ def test_show_class(test_resp_code):
 
 def test_show_update(test_resp_code):
     abc = test_resp_code
-    resp = abc.post('/update', data=dict_update_data)
+    resp = abc.post('/update', data=dict_update_data_no)
+    assert resp.status_code == 302
+
+
+
+def test_show_update(test_resp_code):
+    abc = test_resp_code
+    resp = abc.post('/update', data=dict_update_data_yes)
     assert resp.status_code == 302
 
 
@@ -47,10 +79,16 @@ def test_show_delete(test_resp_code):
     resp = abc.post('/delete', data=dict_delete_data)
     assert resp.status_code == 302
 
+def test_show_delete(test_resp_code):
+    abc = test_resp_code
+    resp = abc.post('/delete', data=dict_delete_data_exception)
+    assert resp.status_code == 302
+
+
 def test_show_update_record(test_resp_code):
     abc = test_resp_code
-    resp = abc.post('/update', data=dict_delete_data)
-    assert resp.status_code == 302
+    resp = abc.post('/updaterecord', data=dict_delete_data)
+    assert resp.status_code == 200
 
 
 
